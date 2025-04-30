49bcd258bf3886486a490905282c62cb7660aa49
Rodney Shen <rodneyshenn@gmail.com>
2025-04-30 10:07:04 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import { redirect } from 'next/dist/server/api-utils';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
   // redirects() {
   //   return [
